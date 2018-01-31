c6497f1363007d5e167b27d2e167f8836791b275
Atul R <atulanand94@gmail.com>
2018-01-31 10:41:51 +0530

    Fix for eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,10 @@
 module.exports = {
-  "extends": "airbnb-base",
-  "env": {
-    "jest": true
-  }
+  extends: 'airbnb-base',
+  env: {
+    jest: true,
+    node: true,
+  },
+  rules: {
+    'import/no-unresolved': [2, { caseSensitive: false }],
+  },
 };
